5caf1e86ffcfed60006ec4c835135ffd36d3aacc
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2016-02-26 14:52:54 -0300

    Add licence file and licence hearders

--- server/controller/collector/collectorpool.js
+++ server/controller/collector/collectorpool.js
@@ -1,3 +1,27 @@
+/****************************************************************************
+**
+** Copyright (C) 2015
+**                     Gustavo Valiati <gustavovaliati@gmail.com>
+**                     Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
+**
+** This file is part of the FishMonitoring project
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; version 2
+** of the License.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**
+****************************************************************************/
+
 var logger = require('winston');
 var PlatformError = require(__base + 'utils/platformerror');
 var Collector = require(__base + 'models/collector');
