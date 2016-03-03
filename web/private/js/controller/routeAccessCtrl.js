eaee1fc0ed118c0487026c86c56a1e0fbb43852e
Mohamad Abu Ali <arabian@brasnet.org>
2016-03-03 12:51:41 -0300

    Removed routeAccess still not used.

--- web/private/js/controller/routeAccessCtrl.js
+++ web/private/js/controller/routeAccessCtrl.js
@@ -2,19 +2,6 @@
 ** @author Mohamad Abu Ali <mohamad@abuali.com.br>
 */
 var app = angular.module('flexApp');
-app.controller('routeAccessCtrl', function($scope, Restangular){
-
-  var routesService = Restangular.service('routes');
-  var usersService = Restangular.service('users');
-
-  routesService.getList().then(function(response){
-		 $scope.routes = response;
-	});
-
-  routesService.getList().then(function(response){
-		 $scope.routes = response;
-	});
-
-  
+app.controller('routeAccessCtrl', function($scope){
 
 });
