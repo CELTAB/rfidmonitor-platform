af22d331acecc6af7e72ef346b47dbd0eb5e70a2
Mohamad Abu Ali <arabian@brasnet.org>
2016-03-17 16:25:07 -0300

    add dashboard values

--- web-dev/private/js/controller/dashboardCtrl.js
+++ web-dev/private/js/controller/dashboardCtrl.js
@@ -5,36 +5,38 @@ var app = angular.module('flexApp');
 app.controller('dashboardCtrl', function($scope, Restangular){
 
 	var collectorsService = Restangular.service('collectors');
+	var rfiddataService = Restangular.service('rfiddatas');
 
+	$scope.labelsChartCollector = [];
+	$scope.dataChartCollector = [];
+	$scope.labelsRfid = [];
+	$scope.dataRfid = [];
+	$scope.seriesRfid = [];
 
   $scope.getCollectors = function(){
-    collectorsService.getList().then(function(response){
-  		 $scope.collectors = response;
+		collectorsService.getList({q: {"dashboard": true}}).then(function(response){
+			response.sort(function sortById(a,b){ return a.id - b.id; });
+		 	$scope.collectors = response;
+
+			if($scope.collectors.length){
+				angular.forEach($scope.collectors, function(collector){
+					$scope.seriesRfid.push(collector.name);
+					$scope.dataChartCollector.push(collector.records.total);
+
+					var lastYear = collector.records.lastYear;
+					var last = [];
+					for(attr in lastYear){
+						last.push(lastYear[attr]);
+					}
+					$scope.dataRfid.push(last);
+				});
+				for(attr in $scope.collectors[0].records.lastYear){
+					$scope.labelsRfid.push(attr);
+				}
+			}
   	});
-  }
+  };
 
 	$scope.getCollectors();
-
-	$scope.labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'];
-	$scope.series = ['Nov/2014', 'Nov/2015', 'Nov/2014', 'Nov/2015'];
-	$scope.data = [
-	  [65, 59, 80, 81, 56, 55, 40, 28, 48, 40, 19, 86, 27, 90, 81, 56, 55, 40, 28, 48, 40, 19, 28, 48, 40, 19, 86, 27, 90, 81],
-	  [28, 48, 40, 19, 86, 27, 90, 40, 19, 86, 27, 90, 81, 56, 55, 40, 28, 48, 40, 19, 28, 14, 12, 10, 18, 60, 80, 33, 14, 70],
-	  [48, 40, 19, 28, 48, 40, 19, 86, 27, 90, 81, 48, 40, 19, 86, 27, 90, 81, 56, 55, 40, 28, 65, 59, 80, 81, 56, 55, 40, 28],
-	  [86, 27, 90, 81, 56, 55, 40, 28, 48, 40, 19, 86, 27, 90, 81, 56, 55, 40, 28, 48, 28, 48, 40, 19, 86, 27, 90, 40, 19, 70]
-	];
-
-	$scope.labelsChartRfid = [ 'jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez' ];
-	$scope.dataChartRfid = [
-	  [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 90, 80],
-	  [28, 48, 40, 19, 86, 27, 90, 40, 19, 86, 27, 90]
-	];
-
-	$scope.labelsChartCollector = ['ABC', 'DFG', 'XYZ', 'HIJ', 'ZZZ'];
-	$scope.dataChartCollector = [1200, 400, 800, 200, 100];
-
-	$scope.onClick = function (points, evt) {
-	  console.log(points, evt);
-	};
 
 });
